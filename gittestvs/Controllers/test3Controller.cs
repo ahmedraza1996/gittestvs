f3b010fad958fb8ccfea25e42197723274a7f0c1
Ahmed <ahmedrazamehmood@gmail.com>
2020-05-31 17:26:31 +0500

    test 3 added

---
+++ gittestvs/Controllers/test3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace gittestvs.Controllers
+{
+    public class test3Controller : ApiController
+    {
+        public string ABC()
+        {
+            return "abc";
+        }
+    }
+}
