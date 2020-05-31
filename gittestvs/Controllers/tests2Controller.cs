8726d980ad91a9a2d963cddb7da3af8d2b9a7194
Ahmed Raza <ahmedrazamehmood@gmail.com>
2020-05-31 16:35:50 +0500

    tests2 added

---
+++ gittestvs/Controllers/tests2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gittestvs.Controllers
+{
+    public class tests2Controller : Controller
+    {
+        // GET: tests2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
